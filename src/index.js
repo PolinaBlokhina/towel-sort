4bc6020737e5ef1d2fddbe5af9c56269fba33e9f
PolinaBlokhina <71521548+PolinaBlokhina@users.noreply.github.com>
2020-10-10 16:07:43 +0300

    feat: empty matrix fix

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    var n = matrix.length;
     ans = [];
-    for (var i = 0; i < n; i++) {
-        if (i % 2 == 0) {
-            for (var j = 0; j < matrix[i].length; j++) {
-                ans[ans.length] = matrix[i][j]
+    if ((matrix)&&(matrix.length > 0)) {
+        ans = [];
+        var n = matrix.length;
+        for (var i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                for (var j = 0; j < matrix[i].length; j++) {
+                    ans[ans.length] = matrix[i][j]
+                }
+            }
+            else {
+                for (var j = matrix[i].length - 1; j >= 0; j--) {
+                    ans[ans.length] = matrix[i][j]
+                }
             }
         }
-        else {
-            for (var j = matrix[i].length - 1; j >= 0; j--) {
-                ans[ans.length] = matrix[i][j]
-            }
-        }
+    }
+    else {
+        ans = [];
     }
     return ans;
 }
